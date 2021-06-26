45d6b9280705850feb30e6e7c0fb7d62e1ebf31c
Xico Aires <xicojr@gmail.com>
2021-06-26 18:28:29 -0300

    Aula 3

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { RelatorioDespesaComponent } from './despesa/relatorio-despesa/relatorio-despesa.component';
+import { RelatorioReceitaComponent } from './receita/relatorio-receita/relatorio-receita.component';
+import { CadastrarUsuarioComponent } from './formulario/cadastrar-usuario/cadastrar-usuario.component';
+import { pathToFileURL } from 'url';
+import { RecuperarSenhaComponent } from './formulario/recuperar-senha/recuperar-senha.component';
+import { CadastrosComponent } from './formulario/cadastros/cadastros.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'relatorio-despesa', component: RelatorioDespesaComponent},
+  {path: 'relatorio-receita', component: RelatorioReceitaComponent},
+  {path: 'cadastrar-usuario', component: CadastrarUsuarioComponent},
+  {path: 'recuperar-senha', component: RecuperarSenhaComponent},
+  {path: 'cadastros', component: CadastrosComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
