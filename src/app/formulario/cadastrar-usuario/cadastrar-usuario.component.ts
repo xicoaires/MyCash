8ecd4fddedf14bac87043ff2298e965b754a0a1b
Francisco Aires <xicojr@gmail.com>
2021-07-03 17:16:41 -0300

    Aula 03/07/21

--- src/app/formulario/cadastrar-usuario/cadastrar-usuario.component.ts
+++ src/app/formulario/cadastrar-usuario/cadastrar-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar-usuario',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarUsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  backToLogin(){
+    this.router.navigate(['/login']);
   }
 
 }
--- src/app/formulario/recuperar-senha/recuperar-senha.component.ts
+++ src/app/formulario/recuperar-senha/recuperar-senha.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar-senha',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarSenhaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  backToLogin(){
+    this.router.navigate(['/login']);
   }
 
 }
